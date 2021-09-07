b867e183c0b2d75c6ab8c9b5fcb54ecc9c4748b2
Carlos Eduardo Fontes de Assis <rockadu@hotmail.com>
2021-09-07 11:27:31 -0300

    Adição de filtro dinamico na consulta

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,16 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  _filtroLista: string = "";
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
   eventos: any = [];
   imagemLargura: number = 50;
   imagemMargem: number = 2;
   mostrarImagem: boolean = false;
-  filtroLista = '';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 
   alternarImagem(){
@@ -26,7 +43,7 @@ export class EventosComponent implements OnInit {
 
   getEventos(){
     this.http.get('http://localhost:5000/api/values').subscribe(
-      response => { this.eventos = response; },
+      response => { this.eventos = this.eventosFiltrados = response; },
       error => { console.log(error);}
     );
   }
